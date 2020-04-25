5f70bf51cae96b9f761cc9bceafcfeb43c5dcbac
Pallavi Kumari <kumaripallavi168@gmail.com>
2020-04-25 14:10:50 +0530

    v0.0.13

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), '..', 'static').replace('\\', '/')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     '/home/simpleblogging/blogapp/static',
+# ]
